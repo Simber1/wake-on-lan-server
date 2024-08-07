5e475d770024216ada28cd693ab5e16e443ef207
Peter Stather <peter.stather@g-portal.com>
2024-08-07 17:15:49 +0100

    initial commit

---
+++ wol-server/app.py
@@ -0,0 +1,18 @@
+from litestar.response import Template
+from litestar import Litestar, get
+from pathlib import Path
+from litestar.contrib.jinja import JinjaTemplateEngine
+from litestar.template.config import TemplateConfig
+import wakeonlan
+
+
+@get("/")
+async def index() -> Template:
+    print("Test1")
+    wakeonlan.send_magic_packet('08:BF:B8:12:EE:CE')
+    print("Test2")
+    return Template(template_name="index.html.jinja2")
+
+template_config = TemplateConfig(directory=Path("templates"), engine=JinjaTemplateEngine)
+app = Litestar([index], template_config=template_config)
+print("Server running!")
